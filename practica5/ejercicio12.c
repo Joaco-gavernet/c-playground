b54e670752635e545f8940a64a4a8f5833457de4
Joaquin Gavernet <joaquingavernet@gmail.com>
2023-05-24 15:29:50 -0300

    ejercicio 12 version inicial, aun no registra desplazamiento en fscanf

--- practica5/ejercicio12.c
+++ practica5/ejercicio12.c
@@ -1,29 +1,42 @@
 #include <stdio.h>
+#include <string.h>
+
 #define forn(i, n) for(int i = 0; i < n; i++)
 #define LONG 7
+
+typedef struct {
+  char dni[10];
+  int posicionBytes;
+} Persona;
 
 int main() {
   FILE * origen = fopen("./recursos/personas2.csv", "r");
   FILE * destino = fopen("./recursos/personas.idx", "w");
 
-  int tabla[LONG] = {0}; // vector de cadenas de caracteres (DNIs)
-  int id = 0;
-  char example[100];
-
-  fscanf(origen, "%s", example);
-  printf("Ultimo leido en .csv %s\n", example);
-
-  while (!feof(origen)) {
-    fscanf(origen, "%d;", &id);
-    fscanf(origen, "%d;", &tabla[id]);
-    printf("ID %d: DNI %d\n", id, tabla[id]);
-
-    fscanf(origen, "%[^\n]s", example);
-    printf("Ultimo leido en .csv %s\n", example);
+  if (origen == NULL || destino == NULL) {
+    printf("Error al abrir los archivos\n");
+    return 1;
   }
 
-  for (int i = 1; i <= id; i++) fprintf(destino, "%d | %d\n", i, tabla[i]);
-  
+  Persona tabla[LONG]; // vector de cadenas de caracteres (DNIs)
+  int id = 0;
+  char aux[100];
+  int desplazamiento = 0;
+
+  desplazamiento = fscanf(origen, "%s", aux);
+  printf("Desplazamiento %d\n", desplazamiento);
+
+  while (!feof(origen)) {
+    tabla[id].posicionBytes = desplazamiento;
+    desplazamiento += fscanf(origen, "%d%c", &id); // leo el numero y luego proceso el ";" que lo acompaña
+    desplazamiento += fscanf(origen, "%[^;]s", tabla[id].dni); // leo hasta el ";" y guardo en tabla[id].dni
+    printf("ID %d: DNI %s\n", id, tabla[id].dni); // proceso el resto de la informacion para desplazar el puntero de fichero
+
+    desplazamiento += fscanf(origen, "%[^\n]s", aux);
+  }
+
+  for (int i = 1; i <= id; i++) fprintf(destino, "%d | %s\n", i, tabla[i]);
+
   fclose(origen);
   fclose(destino);
   return 0;
