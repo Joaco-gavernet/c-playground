a25dea360e151e52ae6468bda5ee646a7a760358
Joaquin Gavernet <joaquingavernet@gmail.com>
2023-06-07 09:29:05 -0300

    Se corrige ejercicio de lectura en .txt y escritura en archivo .dat

--- modulo2/parcial1/ejercicio5.c
+++ modulo2/parcial1/ejercicio5.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct destino {
   int codProv;
@@ -9,25 +10,37 @@ struct destino {
 
 int main() {
   FILE * f = fopen("Habitantes.txt", "r");
-  FILE * n = fopen("Habitantes.dat", "w");
+  FILE * n = fopen("Habitantes.dat", "wb+"); // recordar revisar el metodo de apertura del archivo
   if (f == NULL || n == NULL) {
     printf("No se pudo abrir el archivo");
     return -1;
   }
+  
+  struct destino * habitantes;
+  struct destino aux;
+  int diml = 0;
 
-  struct destino act, max;
-  fscanf(f, "%d %d %s %d", &act.codProv, &act.codLoc, act.nomLoc, &act.nHab);
-  max.nHab = 0;
-
-  while (!feof(f)) {
-    fwrite(&act, sizeof(struct destino), 1, n);
-    if (act.nHab > max.nHab) max = act;
-    fscanf(f, "%d %d %s %d", &act.codProv, &act.codLoc, act.nomLoc, &act.nHab);
+  while (fscanf(f, "%d %d %s %d", &aux.codProv, &aux.codLoc, aux.nomLoc, &aux.nHab) != EOF) {
+    fwrite(&aux, sizeof(struct destino), 1, n); // actualizo .dat mientras proceso .txt
+    diml++;
   }
 
-  printf("Mayor n habitantes en localidad %d %s", max.codLoc, max.nomLoc);
-
   fclose(f);
-  fclose(n);
+
+  // proceso al finalizar
+  rewind(n); // fseek(n, 0, SEEK_SET);
+  habitantes = (struct destino *) calloc(diml, sizeof(struct destino));
+  fread(habitantes, diml, sizeof(struct destino), n); // se recupera informacion del .dat en vector dinamico
+
+  struct destino maxHab = habitantes[0]; // se inicializa el maximo en el primer elemento
+  for (int i = 0; i < diml; i++) {
+    if (habitantes[i].nHab > maxHab.nHab) {
+      maxHab = habitantes[i];
+    }
+  }
+  fclose(n); // recordar cerrar archivo cuando finaliza su uso
+
+  printf("Localidad %s con codigo %d cuenta con la mayor cantidad de habitantes", maxHab.nomLoc, maxHab.codLoc);
+
   return 0;
 }
